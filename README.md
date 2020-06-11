a15c31a64f6e619af540c5a48924bcc670f96237
taylordinkins <xdinkins@gmail.com>
2020-06-10 17:08:58 -0700

    updated

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Variable-Elimination-SAT
 
-In this project we use a SAT solver tool to select a Variable Elimination ordering thathas the best performance for performing future inference queries to the model.
+In this project we use a SAT solver tool to select a Variable Elimination ordering that has the best performance for performing future inference queries to the model.
 
-Variable Elimination can be considered a form of dynamic programming wherewe sum out the variables which are not of interest to us to get the marginal distri-bution of the variables in the query.  To sum up a given variable we find a subsetof factors that mention the given variable. The complexity of this process is expo-nential in the maximum size of the subset formed during the process of summingthe factors.  The complexity can be reduced significantly if we choose the rightvariable elimination order. 
+Variable Elimination can be considered a form of dynamic programming where we sum out the variables which are not of interest to us to get the marginal distribution of the variables in the query.  To sum up a given variable we find a subset of factors that mention the given variable. The complexity of this process is exponential in the maximum size of the subset formed during the process of summing the factors.  The complexity can be reduced significantly if we choose the right variable elimination order. 
+
+Instructions:
+
+	> python main.py --help --experiments <True/False> --brute <True/False> --dataset <celeba/mushroom>
+
+	--help gives an argument summary.
+
+	--dataset will specify which dataset to use. This should be run alone (no --experiments or --brute flags) The files mushroom_atts.csv, mushroom_edges.csv can be modified for graph structure on mushroom data. Similarly, celeb_atts.csv and celeb_edges.csv can be modified for the celeba data.
+
+	--experiments will run sections 6.1 and 6.2 from the report (SAT runtime analysis). If True and --brute is False, it will only run this section. By default it runs on celeba data and would require modifications to perform on mushroom data.
+
+	--brute will find the best ordering for the celeba graph specified by the two files celeb_atts.csv and celeb_edges.csv, just as with the --dataset flag, but will use NetworkX instead of SAT
+
+File Modifications:
+	The attribute files, mushroom_atts.csv and celeb_atts.csv, should be in a single row format and contain only names from the headers for the respective data set. Names should be given in no particular order, separated by commas, on one line. This specifies the set of random variables in the graph.
+
+	The edges files, mushroom_edges.csv and celeb_edges.csv, should have an edge a -> b defined as a,b in each row. That is, each row specifies a directed edge from random variable a to random variable b. Only random variables specified in the attributes file associated with a given data set should be listed here. Also, there is no checking for cycles or duplicates, but these should be excluded or else the graph will not function properly. 
--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def load_args():
     parser = argparse.ArgumentParser(description='arguments')
     parser.add_argument('--dataset', default='celeba', type=str)
     parser.add_argument('--experiments', default=False, type=bool)
+    parser.add_argument('--brute', default=False, type=bool)
     args = parser.parse_args()
     return args
 
@@ -70,11 +71,84 @@ def find_widths(file, keep_atts, edges):
         sat_runtimes.append(runtime)
     return np.array(max_widths), cliques, dictionaries, sat_runtimes, total_sat_runtime
 
+def find_widths_brute(file, keep_atts, edges):
+    graph = create_bayes_net(file, keep_atts, edges)
+    N = len(keep_atts)
+    perms = list(itertools.permutations(keep_atts))
+    ATT_DICT = { keep_atts[i] : i+1 for i in range(0, len(keep_atts) ) }
+    mgraph = graph.to_markov_model()
+
+    inference = VariableElimination(mgraph)
+    max_widths = []
+    cliques = []
+    dictionaries = []
+    sat_runtimes = []
+    total_sat_runtime = 0
+    for perm in tqdm.tqdm(perms):
+        adj_matrix = np.zeros((N, N))
+        igraph = inference.induced_graph(list(perm))
+
+        edges = igraph.edges
+        for ed in edges:
+            first, second = ed
+            first = ATT_DICT[first] - 1
+            second = ATT_DICT[second] - 1
+            adj_matrix[first, second] = 1
+            adj_matrix[second, first] = 1
+            adj_matrix[first, first] = 1
+            adj_matrix[second, second] = 1
+        adj_matrix = adj_matrix.astype(bool)
+        max_width = 0
+        curr_clique = []
+        curr_dict = {}
+        runtime = 0
+        start_time = time.time()
+        iwidth = inference.induced_width(list(perm))
+        end_time = time.time()
+        runtime = end_time-start_time
+        total_sat_runtime += runtime
+        max_widths.append(iwidth)
+        cliques.append(curr_clique)
+        dictionaries.append(curr_dict)
+        sat_runtimes.append(runtime)
+    return np.array(max_widths), cliques, dictionaries, sat_runtimes, total_sat_runtime
+
 def main(file, keep_atts, edges):
 
     perms = list(itertools.permutations(keep_atts))
 
     max_widths, cliques, dictionaries, sat_runtimes, total_sat_runtime = find_widths(file, keep_atts, edges)
+    min_tree_width_idx = np.argmin(max_widths)
+    min_tree_width = np.min(max_widths)
+    best_ordering = perms[min_tree_width_idx]
+    best_clique = cliques[min_tree_width_idx]
+    best_dictionary = dictionaries[min_tree_width_idx]
+    graph = create_bayes_net(file, keep_atts, edges)
+    mgraph = graph.to_markov_model()
+    inference = VariableElimination(mgraph)
+
+    # Perform inference using the best ordering 
+    igraph = inference.induced_graph(list(best_ordering))
+    graph_size = len(keep_atts)
+    density = nx.density(igraph)
+    n_edges = igraph.number_of_edges()
+    worst_runtime = np.max(sat_runtimes)
+    print("Graph size ", graph_size)
+    print("Graph density ", density)
+    print("Graph edges ", n_edges)
+    print("The minimum tree width is " , min_tree_width)
+    print ("The best ordering is ", best_ordering)
+    print("The worst case SAT runtime is ", worst_runtime)
+    print("The total SAT runtime is ", total_sat_runtime)
+
+    return graph_size, density, n_edges, worst_runtime, total_sat_runtime
+
+
+def main_brute(file, keep_atts, edges):
+
+    perms = list(itertools.permutations(keep_atts))
+
+    max_widths, cliques, dictionaries, sat_runtimes, total_sat_runtime = find_widths_brute(file, keep_atts, edges)
     min_tree_width_idx = np.argmin(max_widths)
     min_tree_width = np.min(max_widths)
     best_ordering = perms[min_tree_width_idx]
@@ -116,8 +190,6 @@ def experiments(file):
     # NOTE: had to manually add size 8 onto the plot by running in a notebook with modified functions
     # saving everything to lists ran out of RAM...
     # by default, this will run up to size 7 - any higher takes a long time
-    g_sizes.append(8)
-    sat_runtimes.append(722)
     plt.title('SAT Runtime vs Number of Random Variables')
     plt.plot(g_sizes, sat_runtimes, 'r-.')
     plt.xlabel('Number of Variables')
@@ -153,6 +225,28 @@ def experiments(file):
     plt.close('all')
     print('Done')
 
+def brute(file):
+    g_sizes = []
+    sat_runtimes = []
+    for i in range(len(utils.EXPERIMENTAL_ATTS)):
+        graph_size, density, n_edges, worst_runtime, total_runtime = main_brute(file, utils.EXPERIMENTAL_ATTS[i], utils.EXPERIMENTAL_EDGES[i])
+        g_sizes.append(graph_size)
+        sat_runtimes.append(total_runtime)
+
+    plt.title('Brute Runtime vs Number of Random Variables')
+    plt.plot(g_sizes, sat_runtimes, 'r-.')
+    plt.xlabel('Number of Variables')
+    plt.ylabel('Brute Runtime (sec)')
+    plt.savefig('./figures/runtime_brute.png')
+    plt.close('all')
+
+    plt.title('Brute Runtime vs Number of Random Variables (logscale)')
+    plt.plot(np.log(g_sizes), np.log(sat_runtimes), 'g-.')
+    plt.xlabel('Number of Variables')
+    plt.ylabel('Brute Runtime (sec)')
+    plt.savefig('./figures/runtime_log_brute.png')
+    plt.close('all')
+
 
 
 if __name__=='__main__':
@@ -163,6 +257,11 @@ if __name__=='__main__':
         print('Experiments on Celebrity Data', end='\n\n')
         file = './data/list_attr_celeba.csv'
         experiments(file)
+        exit(0)
+    if args.brute:
+        print('Brute on Celebrity Data', end='\n\n')
+        file = './data/list_attr_celeba.csv'
+        brute(file)
         exit(0)
     if args.dataset == 'mushroom':
         print('Mushrooms Data', end='\n\n')
